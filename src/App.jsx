c5037d21cd555e205049c87981cd3534776bdd66
Shwet Kumar <shwetkhuswaha62@gmail.com>
2025-01-23 23:12:09 +0530

    Finded bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import "./App.css";
 import { useContext, useEffect } from "react";
 import { AppContext } from "./context/AppContext";
-import Header from "./components/Header";
-import Blogs from "./components/Blogs";
-import Pagination from "./components/Pagination";
 import { Route, Routes, useSearchParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Home from "./Pages/Home";
-import BlogPage from "./Pages/BlogPage";
-import TagPage from "./Pages/TagPage";
-import CategoryPage from "./Pages/CategoryPage";
+import BlogPage from "../Pages/BlogPage";
+import TagPage from "../Pages/TagPage";
+import CategoryPage from "../Pages/CategoryPage";
 
 export default function App() {
   const { fetchBlogPosts } = useContext(AppContext);
